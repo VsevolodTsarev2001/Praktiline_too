aeb08014ab6e1d2795a28773f778cec0f8f6dfd6
VsevolodTsarev2001 <153904999+VsevolodTsarev2001@users.noreply.github.com>
2024-02-28 11:44:31 +0200

    Add files via upload

---
+++ Praktiline_too_Palgad/Praktiline_too_Palgad/Praktiline_too_Palgad.py
@@ -0,0 +1,20 @@
+from Mymodule import *
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+
+
+
+
+while True:
+    print("0-Naita andmed veerudes\n1-andemete lisamine\n2-Andmete eemaldamine\n3-Kellel on suurim palk\n4-Kellel on vaiksem palk\n")
+    valik=int(input())
+    if valik==1:
+        inimesed,palgad=lisa_inimesi_ja_palgad(inimesed,palgad,int(input("Mitu inimest lisame? ")))
+    elif valik==0:
+        andmed_veerudes(inimesed, palgad)
+    elif valik==2:
+        andmete_eemaldamine_nimi_jargi(inimesed, palgad)
+    elif valik==3:
+        kellel_on_suurim_palk(inimesed, palgad)
+    elif valik==4:
+        kellel_on_vaiksem_palk(inimesed, palgad)
